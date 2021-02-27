b31a30f4011457d6d5e6beaad8ddccbe82ec78e8
Christian Jacolot <christian.jacolot@lph.bzh>
2021-02-27 15:05:51 +0100

    Mongo Engine for messages.

--- server/scratchy_server/resources/messageres.py
+++ server/scratchy_server/resources/messageres.py
@@ -3,29 +3,28 @@ import time
 from flask_restful import Resource, abort, request
 from scratchy_server.model.messageModel import MessageModel
 from scratchy_server.database import database
+import bson
 
 class MessageRes(Resource):
     def get(self, messageId):
-        if not messageId in database["messages"]:
+        try:
+            return MessageModel.objects.get(id=messageId).to_json()
+        except MessageModel.DoesNotExist as ie:
             abort(404)
-
-        return database["messages"][messageId].__dict__
 
     def post(self):
         messageData = request.get_json()
         message = MessageModel()
-        message.id = uuid.uuid4().hex
         message.content = messageData['content'] if 'content' in messageData else ""
         message.author = messageData['author'] if 'author' in messageData else "Unknown author"
-        message.roomId = messageData['roomId'] if 'roomId' in messageData else "0"
-        message.timestamp = time.time()
+        message.roomId = bson.objectid.ObjectId(messageData['roomId']) if 'roomId' in messageData else ObjectId()
 
-        database['messages'][message.id] = message
-        return message.__dict__
+        message = message.save()
+        return { 'id': str(message.id)}        
 
     def delete(self, messageId):
-        if not messageId in database["messages"]:
-            abort(404)
-
-        if messageId in database["messages"]:
-            del database["messages"][messageId]
+        try:
+            MessageModel.objects.get(id=messageId).delete()
+            return {'success':True}
+        except MessageModel.DoesNotExist as ie:
+            abort(404)        
