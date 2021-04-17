d8e4281e1da46a5f60f606056e642113fad5cbe9
toto <Draeggon@protonmail.com>
2021-04-17 18:38:16 +0200

    done the debug message in the room #56

--- server/scratchy_server/resources/roomres.py
+++ server/scratchy_server/resources/roomres.py
@@ -16,21 +16,35 @@ class RoomRes(Resource):
                 abort(404)
         else:        
             try:
-                return Response(RoomModel.objects.get(id=roomId).to_json(), mimetype="application/json", status=200)
+                response = Response(RoomModel.objects.get(id=roomId).to_json(), mimetype="application/json", status=200)
             except IndexError as ie:
                 abort(404)
+            else:
+                logging.debug("here is %s",json.loads(response.get_data())["title"])
+                return response
 
     def post(self):
         roomData = request.get_json()
         room = RoomModel()
+        # room.id = uuid.uuid4().hex
         room.title = roomData['title'] if 'title' in roomData else "Default title"
         room.description = roomData['description'] if 'description' in roomData else "Default description"
+
+        # database['rooms'][room.id] = room
         room = room.save()
+        logging.debug("created %s",room.title)
         return { 'id': str(room.id)}
 
     def delete(self, roomId):
         try:
-            RoomModel.objects.get(id=roomId).delete()
-            return {'success':True}
+            response = RoomModel.objects.get(id=roomId)
         except IndexError as ie:
             abort(404)
+        else:
+            logging.debug("currently deleting %s",json.loads(response.to_json())["title"])
+            try:
+                response.delete()
+            except:
+                return {'success':False}
+            logging.debug("done, %s has been deleted",json.loads(response.to_json())["title"])
+            return {'success':True}
