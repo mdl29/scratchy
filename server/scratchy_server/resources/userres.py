26ac95a3f66393f8f8aa4b6a5469e63c73845037
Atelier Raspberry Pi <57592308+landerneRPi@users.noreply.github.com>
2021-04-17 16:26:38 +0200

    59 fix database import (#60)
    * Add stop.
    
    * #59 Fix database import issue.

--- server/scratchy_server/resources/userres.py
+++ server/scratchy_server/resources/userres.py
@@ -3,7 +3,6 @@ import uuid
 from flask import Response
 from flask_restful import Resource, abort, request
 from scratchy_server.model.userModel import UserModel
-from scratchy_server.database import database
 
 class UserRes(Resource):
     def get(self, userId):
