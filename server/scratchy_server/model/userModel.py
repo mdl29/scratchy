ba2313c6568f337a377e69878364427ad2b58393
toto <Draeggon@protonmail.com>
2021-04-20 20:07:50 +0200

    closed #69

--- server/scratchy_server/model/userModel.py
+++ server/scratchy_server/model/userModel.py
@@ -3,7 +3,7 @@ from scratchy_server import db_scratchy
 class UserModel(db_scratchy.Document):
     pseudo = db_scratchy.StringField()
     profilImage = db_scratchy.StringField()
-    user = db_scratchy.StringField()
+    user = db_scratchy.StringField(unique = True)
     roomsId = db_scratchy.ListField(db_scratchy.ObjectIdField())
 
     
