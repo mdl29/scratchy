5bbdee74182efaeb5e53a26ba073af3443b8ebb0
viandox <43315863+js-all@users.noreply.github.com>
2021-05-01 18:08:32 +0200

    Fixes #90 gather all components (#116)
    * Fixes #90 i did it !
    
    * rebase and fix bug mentionned in the PR
    
    * separate components in different js files (for when we will switch to .vue)
    
    * fix onInput not existing
    
    * fixes the 20-30 convs on PR #90

---
+++ front/js/room_list.js
@@ -0,0 +1,14 @@
+const roomList = {
+    name: "room-list",
+    props: ["rooms"],
+    emits: ["roomSelected", "roomQuit"],
+    template: `
+    <div class='room_list_wrapper'>
+        <template v-for="(room, index) in rooms" :key="room._id.$oid">
+            <div class="room_list_item">
+                <div class="room_list_room" @click="$emit('roomSelected', {room: room, index: index})"> {{room.title}} </div>
+                <div class="room_list_quit" @click="$emit('roomQuit', {room: room, index: index})"><div></div></div>
+            </div>
+        </template>
+    </div>`
+};
