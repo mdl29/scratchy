4a6af2cc43a37369be51e872cdf7bd4f8104280f
yannis-mlgrn <yannismalgorn@gmail.com>
2021-05-18 14:42:30 +0200

    make reviews

--- front/js/service.js
+++ front/js/service.js
@@ -3,20 +3,14 @@
     var api = new ScratchyService("http://localhost:5000/api");
 - call a function :
     api.function();|
+- add axios cdn link in index.html
+     	<script src="https://unpkg.com/axios@0.21.1/dist/axios.min.js"></script>
+- start scratchy service : 
+    var api = new ScratchyService("http://localhost:5000/api");
+    api;
+- call a function :
+    api.function();|
 */
-/**
- * Scratchy service variable doc :
- * @constructor
- * @param {string} apiUrl - Scratchy service API URL, eg: "http://localhost:5000"
- * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
- * @param {string} roomTitle - title of a room , eg:my room
- * @param {string} roomDescription - description of a room, eg:my room description
- * @param {string} userPseudo - pseudo of a user , eg:toto
- * @param {string} userID - user id , eg:60895dd62d1a706830c31f10
- * @param {string} userProfileImage - profile image link , eg:https://myprofileimage.test/picture.png
- * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
- * @param {string} messageContent - message content , eg: my message content
- */
 
 function ScratchyService(apiUrl) {
     let _this = this;
@@ -24,93 +18,175 @@ function ScratchyService(apiUrl) {
 
 //                               ROOM FUNCTIONS
 
+
+    /**
+     * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
+     * @param {string} roomTitle - title of a room , eg:my room
+     * @param {string} roomDescription - description of a room, eg:my room description
+     * @returns {string} - room id, title and description
+     */
     this.updateRoom = async function(roomId,roomTitle,roomDescription){
         const reponse = await axios.put(apiUrl+'/room/'+roomId, { title: roomTitle , description: roomDescription });
         return reponse.data;
     };
 
+    /**
+     * 
+     * @param {string} roomTitle - title of a room, eg:my room
+     * @param {string} roomDescription - description of a room, eg:my room description
+     * @returns {string} - room id 
+     */
     this.createRoom = async function(roomTitle,roomDescription){
         const reponse = await axios.post(apiUrl+'/room', { title: roomTitle , description: roomDescription });
         return reponse.data;
     };
 
     this.deleteRoom = async function(roomID){
-
         const reponse = await axios.delete('http://localhost:5000/api/room/'+roomID);
-        return reponse.data;
     };
 
     this.getAllRooms = async function(){ // put the id between "" 
-
-        let reponse = await axios.get(apiUrl+'/room'); // make the GET request;
-        // api.getAllRooms().then( (data) => console.log(data));
+        let reponse = await axios.get(apiUrl+'/room'); // make the GET request
         return reponse.data;
     };
 
     this.getRoom = async function(roomID){ // put the id between "" 
             // Make a request for a user with a given ID
             let reponse = await axios.get(apiUrl+'/room/'+roomID);// make the GET request
-            // api.getRoom().then( (data) => console.log(data));
-            return reponse.data;// return data JSON           
+    };
+    /**
+     * 
+     * @param {string} roomID - room id , eg: 60895dd62d1a706830c31f10
+     */
+    this.deleteRoom = async function(roomID){
+        const reponse = await axios.delete(apiUrl+'/room/'+roomID);
+    };
+    /**
+     * 
+     * @returns {array} - all rooms information 
+     */
+    this.getAllRooms = async function(){ // put the id between "" 
+        let reponse = await axios.get(apiUrl+'/room'); // make the GET request;
+        return reponse.data;
+    };
+
+    /**
+     * 
+     * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
+     * @returns {Room} room information , { oid:"60895dd62d1a706830c31f10" ,title:"example"}
+     */
+    this.getRoom = async function(roomID){ // put the id between "" 
+            // Make a request for a user with a given ID
+            let reponse = await axios.get(apiUrl+'/room/'+roomID);// make the GET request
+            return reponse.data; // return data JSON           
     };
 
 //                               USER FUNCTIONS
-
+    
+    /**
+    * 
+    * @param {string} userPseudo - a user pseudo , eg:toto
+    * @returns {User} - user information 
+    */
     this.getUserByPseudo = async function(userPseudo){ // put the id between "" 
         // Make a request for a user with a given ID
         let reponse = await axios.get(apiUrl+'/user?pseudo='+userPseudo);// make the GET request
         return reponse.data;// return data JSON         
     };
 
+    /**
+     * 
+     * @param {string} userID , user id , eg:60895dd62d1a706830c31f10
+     * @returns {User} - user information
+     */
     this.getUserByid = async function(userID){ // put the id between "" 
         // Make a request for a user with a given ID
         let reponse = await axios.get(apiUrl+'/user/'+userID) // make the GET request
         return reponse.data;// return data JSON          
     };
 
+    /**
+    * 
+    * @param {string} userPseudo - a user pseudo , eg:toto
+    * @param {string} userProfileImage - profile image link , eg:https://myprofileimage.test/picture.png
+    * @returns {string} - user id , eg: 60895dd62d1a706830c31f10
+    */
     this.createUser = async function(userPseudo,userProfileImage){
         const reponse = await axios.post(apiUrl+'/user', { pseudo: userPseudo , profileImage: userProfileImage });
         return reponse.data;
     };
 
+    /**
+    * 
+    * @param {string} userID - user id , eg:60895dd62d1a706830c31f10
+    */
     this.deleteUser = async function(userID){
-        // 
-        const reponse = await axios.delete('http://localhost:5000/api/user/'+userID);
-        return reponse.data;
+        const reponse = await axios.delete(apiUrl+'/user/'+userID);
     };
 
+   /**
+     * 
+     * @param {string} userID - user id , eg:60895dd62d1a706830c31f10
+     * @param {string} userPseudo - user pseudo , eg:toto
+     * @returns {string} - user id , eg:60895dd62d1a706830c31f10
+     */
     this.updateUser = async function(userID,userPseudo){
         const reponse = await axios.put(apiUrl+'/user/'+userID, { pseudo: userPseudo});
         return reponse.data;
     };
-
-
 //                               MESSAGE FUNCTIONS
 
+    /**
+     * 
+     * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
+     * @returns {Message} message information 
+     */
     this.getMessage = async function(messageID){ // put the id between "" 
         // Make a request for a user with a given ID
         let reponse = await axios.get(apiUrl+'/message/'+messageID) // make the GET request
         return reponse.data;// return data JSON  
 
     };
+
+    /**
+     * 
+     * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
+     * @returns {array} message information
+     */
     this.getAllMessagesInRoom = async function(roomID){ // put the id between "" 
         // Make a request for a user with a given ID
         let reponse = await axios.get(apiUrl+'/message?roomid='+roomID) // make the GET request
         return reponse.data;// return data JSON 
     };
+
+    /**
+     * 
+     * @param {string} authorID - author id , eg:60895dd62d1a706830c31f10
+     * @param {string} messageContent - message content , eg:my message content
+     * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
+     * @returns {string} message id
+     */
     this.createMessage = async function(authorID,messageContent,roomID){
         const reponse = await axios.post(apiUrl+'/message', { author : authorID , content : messageContent , roomId : roomID });
         return reponse.data;
     };
+
+    /**
+     * 
+     * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
+     * @param {string} messageContent - message content , eg:my message content 
+     * @returns {string} message id
+     */
     this.updateMessage = async function(messageID,messageContent){
         const reponse = await axios.put(apiUrl+'/message/'+messageID, { content: messageContent });
         return reponse.data;
     };
+
+    /**
+     * 
+     * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
+     */
     this.deleteMessage = async function(messageID){
-        const reponse = await axios.delete('http://localhost:5000/api/message/'+messageID);
-        return reponse.data;
+        const reponse = await axios.delete(apiUrl+'/message/'+messageID);
     };
 };
-
-var api = new ScratchyService("http://localhost:5000/api");
-api;
