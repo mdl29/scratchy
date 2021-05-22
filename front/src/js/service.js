ff92f272516fe82c336deea86b2b943bfd8fa54f
Benjamin BERNARD <benjamin.bernard@benvii.com>
2021-05-22 15:33:26 +0200

    :art: Fixes #91, correct JSDoc

--- front/src/js/service.js
+++ front/src/js/service.js
@@ -3,6 +3,7 @@
  * @see {@link https://github.com/mdl29/scratchy|GitHub}
  * @requires module:axios/axios
  */
+
 /*
 - add axios cdn link in index.html :
      	<script src="https://unpkg.com/axios@0.21.1/dist/axios.min.js"></script>
@@ -14,6 +15,36 @@
 - call a function :
     api.function();|
 */
+
+/**
+ * @typedef {Object} Room
+ * @property {string} id - Room unique ID.
+ * @property {string} description - The room description.
+ * @property {string} title - The room title.
+ * @property {string[]} users - List of users IDs.
+ */
+
+/**
+ * @typedef {Object} AllRoom
+ * @property {Room[]} List of rooms.
+ */
+
+/**
+ * @typedef {Object} Message
+ * @property {string} id - Message unique ID.
+ * @property {string} authorID - author id , eg:60895dd62d1a706830c31f10
+ * @property {string} content - message content , eg:my message content
+ * @property {string} roomID - room id , eg:60895dd62d1a706830c31f10
+ */
+
+ /**
+ * @typedef {Object} User
+ * @property {string} id - User unique ID.
+ * @property {string} pseudo - user nickname / pseudo
+ * @property {string} profileImage - URL of the profile image.
+ * @property {string[]} rooms - List of room IDs the user belongs to.
+ */
+
 /**
  * 
  * @constructor
@@ -30,11 +61,10 @@ function ScratchyService(apiUrl) {
      *
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Room
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
      * @param {string} roomTitle - title of a room , eg:my room
      * @param {string} roomDescription - description of a room, eg:my room description
-     * @returns {Room} - room information , eg:{ oid:"60895dd62d1a706830c31f10" ,title:"example", description : "my description"}
+     * @returns {Promise<Room>} - room information , eg:{ oid:"60895dd62d1a706830c31f10" ,title:"example", description : "my description"}
      */
     this.updateRoom = async function(roomId,roomTitle,roomDescription){
         const reponse = await axios.put(apiUrl+'/room/'+roomId, { title: roomTitle , description: roomDescription });
@@ -45,11 +75,10 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Room
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
      * @param {string} roomTitle - title of a room, eg:my room
      * @param {string} roomDescription - description of a room, eg:my room description
-     * @returns {Room} - room information , eg:{ oid:"60895dd62d1a706830c31f10" ,title:"example", description : "my description"}
+     * @returns {Promise<Room>} - room information , eg:{ oid:"60895dd62d1a706830c31f10" ,title:"example", description : "my description"}
      */
     this.createRoom = async function(roomTitle,roomDescription){
         const reponse = await axios.post(apiUrl+'/room', { title: roomTitle , description: roomDescription });
@@ -60,9 +89,8 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Room
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
-     * @returns {Room} room information , eg:{ oid:"60895dd62d1a706830c31f10" ,title:"example"}
+     * @returns {Promise<Room>} room information , eg:{ oid:"60895dd62d1a706830c31f10" ,title:"example"}
      */
     this.getRoom = async function(roomID){ // put the id between "" 
             // Make a request for a user with a given ID
@@ -75,18 +103,18 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Room
+     * @return {Promise} Empty promise.
      */
      this.deleteRoom = async function(roomID){
         const reponse = await axios.delete(apiUrl+'/room/'+roomID);
+        return reponse.data;
     };
 
     /**
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Room
-     * @returns {array} - all rooms information 
+     * @returns {Promise<AllRoom>} - all rooms information 
      */
     this.getAllRooms = async function(){ // put the id between "" 
         let reponse = await axios.get(apiUrl+'/room'); // make the GET request;
@@ -99,9 +127,8 @@ function ScratchyService(apiUrl) {
     * 
     * @async
     * @augments ScratchyService
-    * @typedef {Object} User
     * @param {string} userPseudo - a user pseudo , eg:toto
-    * @returns {User} - user information 
+    * @returns {Promise<User>} - user information 
     */
     this.getUserByPseudo = async function(userPseudo){ // put the id between "" 
         // Make a request for a user with a given ID
@@ -113,9 +140,8 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} User
      * @param {string} userID - user id , eg:60895dd62d1a706830c31f10
-     * @returns {User} - user information
+     * @returns {Promise<User>} - user information
      */
     this.getUserByid = async function(userID){ // put the id between "" 
         // Make a request for a user with a given ID
@@ -127,10 +153,9 @@ function ScratchyService(apiUrl) {
     * 
     * @async
     * @augments ScratchyService
-    * @typedef {Object} User
     * @param {string} userPseudo - a user pseudo , eg:toto
     * @param {string} userProfileImage - profile image link , eg:https://myprofileimage.test/picture.png
-    * @returns {string} - user id , eg: 60895dd62d1a706830c31f10
+    * @returns {Promise<User>} - user id , eg: 60895dd62d1a706830c31f10
     */
     this.createUser = async function(userPseudo,userProfileImage){
         const reponse = await axios.post(apiUrl+'/user', { pseudo: userPseudo , profileImage: userProfileImage });
@@ -141,21 +166,21 @@ function ScratchyService(apiUrl) {
     * 
     * @async
     * @augments ScratchyService
-    * @typedef {Object} User
+    * @return {Promise} Empty promise.
     */
     this.deleteUser = async function(userID){
         const reponse = await axios.delete(apiUrl+'/user/'+userID);
+        return reponse;
     };
 
    /**
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} User
      * @param {string} userID - user id , eg:60895dd62d1a706830c31f10
      * @param {string} userPseudo - user pseudo , eg:toto
      * @param {string} userProfileImage - profile image link , eg:https://myprofileimage.test/picture.png
-     * @returns {string} - user id , eg:60895dd62d1a706830c31f10
+     * @returns {Promise<User>} - user id , eg:60895dd62d1a706830c31f10
      */
     this.updateUser = async function(userID,userPseudo,userProfileImage){
         const reponse = await axios.put(apiUrl+'/user/'+userID, { pseudo: userPseudo , profileImage : userProfileImage});
@@ -167,9 +192,8 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Message
      * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
-     * @returns {Message} message information 
+     * @returns {Promise<Message>} message information 
      */
     this.getMessage = async function(messageID){ // put the id between "" 
         // Make a request for a user with a given ID
@@ -182,9 +206,8 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Message
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
-     * @returns {array} message information
+     * @returns {Promise<Message[]>} message information. Might be a bug here.
      */
     this.getAllMessagesInRoom = async function(roomID){ // put the id between "" 
         // Make a request for a user with a given ID
@@ -196,11 +219,10 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Message
      * @param {string} authorID - author id , eg:60895dd62d1a706830c31f10
      * @param {string} messageContent - message content , eg:my message content
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
-     * @returns {Message} message information, eg: author : 60895dd62d1a706830c31f10 , content : hello world , roomId : 60895dd56d1a706830c31f16 
+     * @returns {Promise<Message>} message information, eg: author : 60895dd62d1a706830c31f10 , content : hello world , roomId : 60895dd56d1a706830c31f16 
      */
     this.createMessage = async function(authorID,messageContent,roomID){
         const reponse = await axios.post(apiUrl+'/message', { author : authorID , content : messageContent , roomId : roomID });
@@ -211,10 +233,11 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Message
      * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
+     * @param {string} messageAuthor - author of the message ID , eg:60895dd62d1a706830c31f10
      * @param {string} messageContent - message content , eg:my message content 
-     * @returns {string} message id
+     * @param {string} roomID - Id of the room the message belongs to.
+     * @returns {Promise<Message>} message id
      */
     this.updateMessage = async function(messageID,messageAuthor,messageContent,roomID){
         const reponse = await axios.put(apiUrl+'/message/'+messageID, { author: messageAuthor , content: messageContent , roomId : roomID });
@@ -225,10 +248,11 @@ function ScratchyService(apiUrl) {
      * 
      * @async
      * @augments ScratchyService
-     * @typedef {Object} Message
      * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
+     * @return {Promise} Empty promise.
      */
     this.deleteMessage = async function(messageID){
         const reponse = await axios.delete(apiUrl+'/message/'+messageID);
+        return reponse.data;
     };
 };
