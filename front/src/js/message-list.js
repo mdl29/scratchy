0be4dfe2c9ce70e0a5011b29b5352d02db47fa97
yannis-mlgrn <yannismalgorn@gmail.com>
2021-05-29 18:04:47 +0200

    refresh timestamp in message-list.js file

--- front/src/js/message-list.js
+++ front/src/js/message-list.js
@@ -2,16 +2,30 @@ const messages = {
     name: 'messages',
     props: ['messages'],
     methods: {
-        humanDate(ts){
+        humanDate(ts,now){
             return moment(ts).fromNow();
         }
+    },
+    data: function(){
+        return {
+            now: new Date()
+        };
+    },
+    created() {
+        var self = this
+        setInterval(function () {
+             self.now = new Date();
+        }, 2000)
+    },
+    computed(){
+
     },
     template: `
     <ul class="messages_list_wrapper">
         <li v-for="msg in messages" class="messages_item" :key="msg.id">
             <div class="messages_title">
                 <span class="messages_author"> {{msg.author}} </span>
-                <span class="messages_timestamp"> {{humanDate(msg.timestamp)}} </span>
+                <span class="messages_timestamp"> {{ humanDate(msg.timestamp, now) }} </span>
             </div>
             <div class="messages_content">
                 {{msg.content}}
