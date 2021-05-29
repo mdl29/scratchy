a8c7ba2dcee904281ae300cd1f24a2acb76d216d
Atelier Raspberry Pi <57592308+landerneRPi@users.noreply.github.com>
2021-05-29 17:35:27 +0200

    Fixes #158 : Get messages by roomId #187 (#195)
    * added the command to get messages by roomId
    
    * added integration tests
    
    * Fixes #158 : Get messages by roomId
    
    * Update README.md
    
    Co-authored-by: toto <Draeggon@protonmail.com>

--- server/scratchy_server/model/objectIdSchema.py
+++ server/scratchy_server/model/objectIdSchema.py
@@ -3,6 +3,7 @@ import bson
 
 # created because of incompatibility of marshmallow with object id
 # the code is copy past from: https://github.com/marshmallow-code/marshmallow/issues/1542
+# and i added kwargs arumgent to deserialize
 class ObjectIdSchema(ma.fields.String):
     """
     Marshmallow field for :class:`bson.ObjectId`
@@ -12,7 +13,7 @@ class ObjectIdSchema(ma.fields.String):
             return None
         return str(value)
 
-    def _deserialize(self, value, attr, data):
+    def _deserialize(self, value, attr, data, **kwargs):
         try:
             return bson.ObjectId(value)
         except (TypeError, bson.errors.InvalidId):
