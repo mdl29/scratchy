51ec625ee948886d18dd85ef144a476ef3fec731
yannis-mlgrn <yannismalgorn@gmail.com>
2021-06-02 22:31:03 +0200

    make a popup who give the room id

--- front/src/js/activity_bar.js
+++ front/src/js/activity_bar.js
@@ -2,18 +2,29 @@ const activityBar = {
     name: "activity-bar",
     props: ["currentRoom", "isWriting"],
     methods:{
-        share: function(currentRoom) { 
-            id = this.currentRoom._id.$oid
-            console.log(id);
-            alert("id : " + id);
+        openPopup: function() { 
+            var popup = document.getElementById("popup");
+            popup.classList.toggle("shown")
         }
     },
+    data: () => ({
+        popup: false
+    }),
     template: `
     <div class='activity_bar'>
     <template v-if="currentRoom !== null">
-        <div class='activity_bar_room'>{{currentRoom.title}} <button @click="share(currentRoom)"><i class="material-icons" id="share-icon">share</i></button></div>
+        <div class='activity_bar_room'>{{currentRoom.title}} 
+            <button v-on:click="popup = true" ><img src="icons/share-icon.svg"></button>
+        </div>
         <div v-if="isWriting" class='activity_bar_writing'> writing... </div>
-    
+            <div class="share_popup" v-show="popup">
+                <div class="share_title">Share room id</div>
+                <label class="share_label"> room id : {{currentRoom._id.$oid}}</label>
+                <div class="share_submit_wrapper">
+                <button class="share_submit" v-on:click="popup = false">close</button>
+                </div>
+            </div>
+
     </template>
     <!-- to keep the activity bar rendering correctly even when no room is provided -->
     &nbsp;
