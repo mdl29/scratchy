a316cc9ff9b9baebab5689701160777a2ee4bf89
yannis-mlgrn <yannismalgorn@gmail.com>
2021-06-03 21:10:34 +0200

    replace black share icon by a white

--- front/src/js/activity_bar.js
+++ front/src/js/activity_bar.js
@@ -14,12 +14,12 @@ const activityBar = {
     <div class='activity_bar'>
     <template v-if="currentRoom !== null">
         <div class='activity_bar_room'>{{currentRoom.title}} 
-            <button v-on:click="popup = true" ><img src="icons/share-icon.svg"></button>
+            <button v-on:click="popup = true" class="button_popup"><img src="icons/share-icon-white.svg" id="popup_icon"></button>
         </div>
         <div v-if="isWriting" class='activity_bar_writing'> writing... </div>
             <div class="share_popup" v-show="popup">
                 <div class="share_title">Share room id</div>
-                <label class="share_label"> room id : {{currentRoom._id.$oid}}</label>
+                <label class="share_label"> room id : {{currentRoom._id.$oid}}</label> // replace "currentRoom._id.$oid" by currentRoom._id when 151 will be merge
                 <div class="share_submit_wrapper">
                 <button class="share_submit" v-on:click="popup = false">close</button>
                 </div>
