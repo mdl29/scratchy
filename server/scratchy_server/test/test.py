5e2ddfba0461afc4d3c3ba37e293604b43b6594e
toto <Draeggon@protonmail.com>
2021-06-05 16:45:58 +0200

    Added test hook before every test

--- server/scratchy_server/test/test.py
+++ server/scratchy_server/test/test.py
@@ -2,4 +2,8 @@ from tavern.core import run
 from glob import glob
 
 for test in glob("test_*.yaml"):
-	run(test)
+    run(test)
+
+
+def pytest_tavern_beta_before_every_test_run(test_dict, variables):
+    run("test_deleteAll.tavern.yaml")
