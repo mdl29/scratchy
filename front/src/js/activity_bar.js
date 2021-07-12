171ce29eba13b7f0afc5ba9fa2ee3413327db524
viandoxdev <titouangoulois29@gmail.com>
2021-07-12 21:13:23 +0200

    fixes #223 change button css and reorganize activity bar html

--- front/src/js/activity_bar.js
+++ front/src/js/activity_bar.js
@@ -7,17 +7,25 @@ const activityBar = {
     template: `
     <div class='activity_bar'>
     <template v-if="currentRoom !== null">
-        <div class='activity_bar_room'>{{currentRoom.title}} 
-            <button v-on:click="popup = true" class="button_popup"><img src="icons/share-icon-white.svg" id="popup_icon"></button>
+        <div class='activity_bar_room'>
+            {{currentRoom.title}}    
         </div>
+        <button v-on:click="popup = true" class="button_popup"><img src="icons/share-icon-white.svg" id="popup_icon"></button>
         <div v-if="isWriting" class='activity_bar_writing'> writing... </div>
-            <div class="share_popup" v-show="popup">
+        <div class="share_popup_background" v-if="popup">
+            <div class="share_popup">
                 <div class="share_title">Share room id</div>
-                <label class="share_label"> room id : {{currentRoom.id}}</label>
+                <div class="room_id">
+                    <label class="share_label"> room id: &nbsp;</label>
+                    <span>
+                        {{currentRoom.id}}
+                    </span>
+                </div>
                 <div class="share_submit_wrapper">
                 <button class="share_submit" v-on:click="popup = false">close</button>
                 </div>
             </div>
+        </div>
 
     </template>
     <!-- to keep the activity bar rendering correctly even when no room is provided -->
