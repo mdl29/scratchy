13b5df91e6f38a3cb476455b2c8eca5d1bff455d
Atelier Raspberry Pi <57592308+landerneRPi@users.noreply.github.com>
2021-08-16 22:51:10 +0200

    125 [back] [metrics] add metrics to scratchy. (#245)
    * [back] [metrics] add metrics to scratchy.
    
    * [back] [metrics] add metrics to scratchy.

--- server/scratchy_server/__main__.py
+++ server/scratchy_server/__main__.py
@@ -9,6 +9,7 @@ from flask.cli import cli
 def main():
     os.environ['FLASK_APP'] = 'scratchy_server.app'
     os.environ['FLASK_ENV'] = 'development'
+    os.environ['DEBUG_METRICS'] = "True" # Needed for prometheus to work. See : https://stackoverflow.com/a/66650309 
     sys.exit(cli.main(args=['run', '--host=0.0.0.0'], prog_name="python -m flask"))
 
 
