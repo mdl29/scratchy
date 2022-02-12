c8fe17970e75e9cd9a7feb076457628451a0f650
yannis-mlgrn <yannismalgorn@gmail.com>
2022-02-12 20:08:43 +0100

    ✏ Fixes review of @viandoxdev

--- front/src/components/ActivityBar.vue
+++ front/src/components/ActivityBar.vue
@@ -232,7 +232,7 @@ export default defineComponent({
     background-image: linear-gradient(to right, #25aae1, #40e495, #30dd8a, #2bb673);
     box-shadow: 0 4px 15px 0 rgba(49, 196, 190, 0.75);
 }
-.copy_wrapper[data-v-04a542f8] {
+.copy_wrapper {
   display: flex;
 }
 </style>
